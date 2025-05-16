40ece28d1f5917fe44dce7a204eb95081e6a2971
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-05-16 15:38:41 -0300

    Import novo e erros corrigidos.

--- Sprint 5/Semana 9 - Base versionamento e programação Python/Calculadora/test_calculadora.py
+++ Sprint 5/Semana 9 - Base versionamento e programação Python/Calculadora/test_calculadora.py
@@ -1,26 +1,31 @@
 import pytest
-from calculadora import Calculadora
+from calculadora import soma
+from calculadora import subtracao
+from calculadora import multiplicacao
+from calculadora import divisao
+from calculadora import potenciacao
+from calculadora import media
 
 @pytest.mark.parametrize("n1_soma, n2_soma, resultado_esperado_soma", [(1, 5, 6), (5, 10, 15), (6, 8, 14)])
-def test_soma(n1_soma, n2_soma, resultado_esperado_soma)
+def test_soma(n1_soma, n2_soma, resultado_esperado_soma):
     assert soma(n1_soma, n2_soma) == resultado_esperado_soma
 
 @pytest.mark.parametrize("n1_subtracao, n2_subtracao, resultado_esperado_subtracao", [(8, 6, 2), (5, 2, 3), (16, 8, 8)])
-def test_subtracao(n1_subtracao, n2_subtracao, resultado_esperado_subtracao)
+def test_subtracao(n1_subtracao, n2_subtracao, resultado_esperado_subtracao):
     assert subtracao(n1_subtracao, n2_subtracao) == resultado_esperado_subtracao
 
 @pytest.mark.parametrize("n1_multiplicacao, n2_multiplicacao, resultado_esperado_multiplicacao", [(3, 2, 6), (6, 5, 30), (8, 2, 16)])
-def test_multiplicacao(n1_multiplicacao, n2_multiplicacao, resultado_esperado_multiplicacao)
+def test_multiplicacao(n1_multiplicacao, n2_multiplicacao, resultado_esperado_multiplicacao):
     assert multiplicacao(n1_multiplicacao, n2_multiplicacao) == resultado_esperado_multiplicacao
 
 @pytest.mark.parametrize("n1_divisao, n2_divisao, resultado_esperado_divisao", [(8, 2, 4), (15, 5, 3), (21, 7, 3)])
-def test_divisao(n1_divisao, n2_divisao, resultado_esperado_divisao)
+def test_divisao(n1_divisao, n2_divisao, resultado_esperado_divisao):
     assert divisao(n1_divisao, n2_divisao) == resultado_esperado_divisao
 
-@pytest.mark.parametrize("n1_elevado_quadrado, n2_elevado_quadrado, resultado_esperado_elevado_quadrado", [(2, 2, 4), (10, 2, 100), (4, 2, 16)])
-def test_elevado_quadrado(n1_elevado_quadrado, n2_elevado_quadrado, resultado_esperado_elevado_quadrado)
-    assert elevado_quadrado(n1_elevado_quadrado, n2_elevado_quadrado) == resultado_esperado_elevado_quadrado
+@pytest.mark.parametrize("n1_potenciacao, n2_potenciacao, resultado_esperado_potenciacao", [(2, 2, 4), (10, 2, 100), (4, 2, 16)])
+def test_potenciacao(n1_potenciacao, n2_potenciacao, resultado_esperado_potenciacao):
+    assert potenciacao(n1_potenciacao, n2_potenciacao) == resultado_esperado_potenciacao
 
 @pytest.mark.parametrize("n1_media, n2_media, resultado_esperado_media", [(18, 16, 17), (20, 14, 17), (45, 65, 55)])
-def test_media(n1_media, n2_media, resultado_esperado_media)
+def test_media(n1_media, n2_media, resultado_esperado_media):
     assert media(n1_media, n2_media) == resultado_esperado_media
