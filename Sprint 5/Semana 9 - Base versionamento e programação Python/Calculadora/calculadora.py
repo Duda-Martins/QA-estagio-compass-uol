c3168cef29620101eaa958a68599d21dfd66e8e6
Maria Eduarda <mrodrigues.mariaeduarda@gmail.com>
2025-05-16 14:54:06 -0300

    Criando a classe e os testes.

---
+++ Sprint 5/Semana 9 - Base versionamento e programação Python/Calculadora/calculadora.py
@@ -0,0 +1,14 @@
+# Copiei o esqueleto da classe do Gemini.
+
+class Calculadora:
+    # Define atributos e métodos aqui
+    # def __init__(self, atributo1, atributo2):
+    #     self.atributo1 = atributo1
+    #     self.atributo2 = atributo2
+
+    def soma(self):
+        # Código do método
+        # pass
+
+# Criar uma instância da classe
+objeto = Calculadora("valor do atributo1", "valor do atributo2")
